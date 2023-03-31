acade76bff96671c59544b3a0df16b595a1fff49
PhelipeJ <phelipe_joao@hotmail.com>
2023-03-31 11:57:48 -0300

    craindo class livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -4,13 +4,51 @@ import br.com.dio.model.Gato;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
-            Gato gato = new Gato ();
+        Gato gato = new Gato();
 
+        Livro livro1 = new Livro("O problema dos 3 corpos", 300);
+        System.out.println(livro1);
         System.out.println(gato);
 
 
    /*     int a = 5;
         int b = 3;
         System.out.println("Hello World! " + (a+b));*/
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numpaginas;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumpaginas() {
+        return numpaginas;
+    }
+
+    public void setNumpaginas(Integer numpaginas) {
+        this.numpaginas = numpaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numpaginas=" + numpaginas +
+                '}';
+    }
+
+    public Livro(String nome, Integer numpaginas) {
+        this.nome = nome;
+        this.numpaginas = numpaginas;
+
+
     }
 }
